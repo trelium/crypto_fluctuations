a7a30aa23095f284294d93b982eafccdecfd073e
RiccardoImprota <riccardo.imp@gmail.com>
2021-04-29 10:19:47 +0200

    commit Riccardo

--- src/first testing/requestingapi.py
+++ src/first testing/requestingapi.py
@@ -2,8 +2,8 @@ import requests
 import json
 
 
-response = requests.get("https://api.coingecko.com/api/v3/coins/bitcoin/market_chart?vs_currency=usd&days=1")
+response = requests.get("https://api.coingecko.com/api/v3/coins/bitcoin")
 response=response.json()
-print(response['prices'])
+print(response)
 
-print(len(response['prices']))
+print(len(response))
