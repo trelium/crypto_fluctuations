81edfe87f2c907724c4ab8884e9f3d3695cd9d97
RiccardoImprota <riccardo.imp@gmail.com>
2021-05-13 14:51:03 +0000

    minor improvements to mqtt, needs bugfixing

--- src/sqltesting.py
+++ src/sqltesting.py
@@ -1,24 +1,7 @@
-# import requests
-# import time
+import datetime
 
-# for i in range(10):
-#     response= requests.get("https://api.coingecko.com/api/v3/coins/bitcoin/market_chart?vs_currency=usd&days=200")
-#     response=response.json()
-#     for pricedatapoint in response['prices']:
-#         mqttpublisher('bitcoin',pricedatapoint)
 
-# def mqttpublisher(self,crypto,pricedatapoint):
-        
-#     #mqtt connecter
-#     client = mqtt.Client('scraperclient')
-#     def on_connect(client, userdata, flags, rc):
-#         if rc==0:
-#             print("connected OK Returned code=",rc)
-#         else:
-#             print("Bad connection Returned code=",rc)
-#     client.on_connect = on_connect
-#     client.connect(self.broker, 1883, 60)
-
-#     #mqttpublisher
-#     path='scraper/'+crypto
-#     client.publish(path,str(pricedatapoint), qos=0)
+ts= datetime.date.today()
+ys=datetime.date.fromtimestamp(1603756800000/1000)
+print(ts)
+print(ys)
