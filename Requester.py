5fd33db32fc86dc3eb0b93a20829c8bd30222483
trelium <jacopo.mocellin@gmail.com>
2021-05-20 21:16:58 +0200

    Solved bugs, pfirst stable release

--- Requester.py
+++ Requester.py
@@ -1,3 +1,21 @@
+"""
+-------------------------------------------------------------------
+---- Database Management Utilities for Cryptocurrency Predictor----
+------------------------------------------------------------------- 
+Developed by Jacopo Mocellin, Riccardo Improta 
+University of Trento - May 2021.
+
+---- Description----
+Tis module contains the utility class used in order to interact with 
+the SQL database instance storing user's chat states and preferences 
+via suitable queries.  
+
+Functionalities include:
+    * Setting states relative to different chat_ids
+    * Checking information related to different chat_ids
+
+"""
+
 # ! pip install pycoingecko
 import pyodbc
 from pycoingecko import CoinGeckoAPI
@@ -9,36 +27,57 @@ class UtentiSQL():
     by the users via the Telegram interface. 
     """
     def __init__(self):
-        self.server = 'bdtproject-sqlserver.database.windows.net' #collation: SQL_Latin1_General_CP1_CI_AS
-        self.database = 'bdt-SQL1'
+        self.server = 'bdtproject.database.windows.net'  #collation: SQL_Latin1_General_CP1_CI_AS
+        self.database = 'BDT-sql2'
         self.username = 'jacoccardo'
         self.password = 'Riccarcopo1'   
         self.driver= '{ODBC Driver 17 for SQL Server}'
         
         #establish connection 
         self.cnxn = pyodbc.connect('DRIVER='+self.driver+';SERVER='+self.server+';PORT=1433;DATABASE='+self.database+';UID='+self.username+';PWD='+ self.password)
-        self.cursor = cnxn.cursor()
+        self.cursor = self.cnxn.cursor()
+
+        #check if data is already present, otherwise create table 
+        self.cursor.execute("""SELECT  table_name name FROM INFORMATION_SCHEMA.TABLES """)
+        present_tables = self.cursor.fetchall()
+        if not 'utenti_bot' in [elem for sublist in present_tables for elem in sublist]: #True if table is present
+            self.cursor.execute("""CREATE TABLE utenti_bot 
+            (user_id VARCHAR(300), 
+            chat_id VARCHAR(300), 
+            state VARCHAR(300), 
+            active BINARY(1), 
+            coin_1 VARCHAR(300),
+            pct_1 FLOAT, 
+            coin_2 VARCHAR(300),
+            pct_2 FLOAT,
+            coin_3 VARCHAR(300),
+            pct_3 FLOAT,
+            coin_4 VARCHAR(300),
+            pct_4 FLOAT,
+            coin_5 VARCHAR(300),
+            pct_5 FLOAT);""")
+
     
     def get_chats(self, currentvalues): 
         """
         Retrieves the chat_ids relative to the users that need to be notified of a change, given in input the current market values. 
         """
         for currency in currentvalues:
-            if currency['usd'] > 0:
-                self.cursor.execute("""SELECT chat_id FROM utenti_bot 
-                                        WHERE active = 1 
-                                        AND {} > {} """.format(currency, currency['usd']))
+            if currentvalues[currency]['usd'] > 0:
+                self.cursor.execute("""SELECT chat_id FROM dbo.utenti_bot WHERE active = 1 AND {} > {} """.format(currency, currentvalues[currency]['usd']))
             else:
-                self.cursor.execute("""SELECT chat_id FROM utenti_bot 
+                self.cursor.execute("""SELECT chat_id FROM dbo.utenti_bot 
                                         WHERE active = 1 
-                                        AND {} < {} """.format(currency, currency['usd']))
+                                        AND {} < {} """.format(currency, currentvalues[currency]['usd']))
 
         return self.cursor.fetchall() #list of tuples
 
-    def set_state(self, user:str(), chat:str(), state:str()): #TODO what if the record for a certain chat already exists? 
+    def set_state(self, user:str(), chat:str(), state:str()):  
         try:
-            self.cursor.execute("""INSERT INTO utenti_bot(user_id, chat_id, state) 
-                                    VALUES ('{}','{}','{}')""".format(user, chat, state))
+            self.cursor.execute("""UPDATE dbo.utenti_bot SET state='{state}' WHERE chat_id='{chat}'
+                                IF @@ROWCOUNT=0
+                                INSERT INTO dbo.utenti_bot(user_id, chat_id, state) VALUES ('{user}','{chat}','{state}');""".format(user = user, chat = chat, state = state))
+
             self.cnxn.commit()
             return True
         except: 
@@ -46,20 +85,25 @@ class UtentiSQL():
     
     def get_state(self, chat:str()):
         try:
-            self.cursor.execute("""SELECT state FROM utenti_bot
-                                    WHERE chat_id = {} """.format(chat))
+            self.cursor.execute("""SELECT state FROM dbo.utenti_bot
+                                    WHERE chat_id = '{}' """.format(chat))
             found = self.cursor.fetchall()
             if len(found) != 0:
                 return str(found[0][0])
             else:
-                return ('No state')
+                return ('no state')
         except:
             return False
 
-    def set_active(self,chat:str()):
+    def set_active(self,chat:str(), value:int()):
+        """
+        Marks the user as active (that is: waiting for updates) by setting the respective attribute.
+        :Params:
+        value: 0 = inactive; 1 = active
+        """
         try:
-            self.cursor.execute("""UPDATE utenti_bot SET active = 1 
-                                    WHERE chat_id = '{}' """.format(chat)
+            self.cursor.execute("""UPDATE dbo.utenti_bot SET active = {} 
+                                    WHERE chat_id = '{}' """.format(value, chat))
             self.cnxn.commit()
             return True
         except: 
@@ -67,20 +111,25 @@ class UtentiSQL():
 
     def is_already_present(self,chat:str()):
         """
-        returns true if a chat is already in the database, false otherwise 
+        Returns true if a chat is already in the database, false otherwise 
         """
-        self.cursor.execute("""SELECT chat_id FROM utenti_bot
-                                    WHERE chat_id = {} """.format(chat))
+        self.cursor.execute("""SELECT chat_id FROM dbo.utenti_bot
+                                    WHERE chat_id = '{}' """.format(chat))
         found = self.cursor.fetchall()
         if len(found) != 0:
             return True 
         else:
             return False
 
+    
     def get_active(self,chat:str()):
+        """
+        Returns the current value of the 'active' attribute of a user.
+        Exits with value False in case of errors raised
+        """
         try:
-            self.cursor.execute("""SELECT active FROM utenti_bot
-                                    WHERE chat_id = {} """.format(chat))
+            self.cursor.execute("""SELECT active FROM dbo.utenti_bot
+                                    WHERE chat_id = '{}' """.format(chat))
             found = self.cursor.fetchall()
             if len(found) != 0 and str(found[0][0]) ==  b'\x01':
                 return 1
@@ -90,27 +139,24 @@ class UtentiSQL():
                 return False
         except:
             return False
+        
 
     def set_preferences(self, chat:str(), preferences: dict()):
         try:
-            if not self.is_already_present(chat):
-                    self.cursor.execute("""INSERT INTO utenti_bot(chat_id) 
-                                    VALUES '{}' """.format(chat)
+            if not self.is_already_present(chat): #actually it is always going to be present
+                    self.cursor.execute("""INSERT INTO dbo.utenti_bot(chat_id) 
+                                    #VALUES '{}' """.format(chat))
                     self.cnxn.commit()                        
-            for coin in preferences:
-                self.cursor.execute("""UPDATE utenti_bot SET '{}' = {}
-                                WHERE chat_id = '{}' """.format(coin, preferences[coin], chat)
+            i=0
+            for coin in preferences: #valid only since we have 5 preferences to loop
+                i+=1
+                self.cursor.execute("""UPDATE dbo.utenti_bot 
+                                    SET coin_{i} = '{name}', pct_{i} = {pct}                                    
+                                    WHERE chat_id = '{chat}' """.format(i = i, name = coin, pct = preferences[coin], chat=chat))
                 self.cnxn.commit()
                 
         except:
             raise ValueError()
             
 
-            
-        
 
-cg = CoinGeckoAPI()
-db = UtentiSQL()
-
-currentvalues = cg.get_price(ids=['bitcoin', 'ripple', 'ethereum','Binance Coin','dogecoin' ], vs_currencies='usd')
-db.get_chats(currentvalues)
--- prova2.py
+++ prova2.py
@@ -1,7 +1,7 @@
 import pyodbc
 
-server = 'bdtproject-sqlserver.database.windows.net'
-database = 'bdt-SQL1'
+server = 'bdtproject.database.windows.net'
+database = 'bdt-sql2'
 username = 'jacoccardo'
 password = 'Riccarcopo1'   
 driver= '{ODBC Driver 17 for SQL Server}'
@@ -15,11 +15,14 @@ cursor = cnxn.cursor()
 #cursor.execute("CREATE TABLE utenti_bot (user_id VARCHAR(300), chat_id VARCHAR(300), state VARCHAR(300), active BINARY(1), binancecoin FLOAT, bitcoin FLOAT, ethereum FLOAT, ripple FLOAT, dogecoin FLOAT);") #submits the quey 
 #cursor.execute("INSERT INTO utenti_bot(user_id,state,binancecoin,active) VALUES ('fhrigni56','in attesa',-23.45,0)" )
 #cnxn.commit() #mandatory in order to save permanently 
-cursor.execute("select * from utenti_bot where active = 0 ")
+#cursor.execute("select * from utenti_bot where active = 0 ")
+cursor.execute("SELECT chat_id FROM dbo.utenti_bot WHERE chat_id = '4yyuy44610376' ")
+rows = cursor.fetchall()
+print(rows)
 
 #cursor.execute("CREATE TABLE notifiche_utenti (user_id VARCHAR(300), chat_id VARCHAR(300), last_notified TIMESTAMP, binancecoin FLOAT, bitcoin FLOAT, ethereum FLOAT, ripple FLOAT, dogecoin FLOAT);")  
 #cnxn.commit()
-
+"""
 rows = cursor.fetchall() #gets the query results 
 print(rows[0][3])
 print(bool(rows[0][3]))
@@ -27,6 +30,12 @@ print(rows[0][3] == b'\x00')
 print(bool(rows[0][3]) == True)
 
 print(bool( b'\x01')==False)
+"""
+
+#delete table 
+#cursor.execute("""DROP TABLE utenti_bot;""")
+#cnxn.commit()
+
 """
 with pyodbc.connect('DRIVER='+driver+';SERVER='+server+';PORT=1433;DATABASE='+database+';UID='+username+';PWD='+ password) as conn:
     with conn.cursor() as cursor:
@@ -36,3 +45,13 @@ with pyodbc.connect('DRIVER='+driver+';SERVER='+server+';PORT=1433;DATABASE='+da
             print (str(row[0]) + " " + str(row[1]))
             row = cursor.fetchone()
 """
+
+diction  = {'update_id': 136492356, 'message': {'message_id': 272, 'date': 1621530681, 
+'chat': {'id': 444610376, 'type': 'private', 'username': 'trelium', 'first_name': 'Jacopo'}, 
+'text': '/start', 
+'entities': [{'type': 'bot_command', 'offset': 0, 'length': 6}], 
+'caption_entities': [], 'photo': [], 'new_chat_members': [], 'new_chat_photo': [], 'delete_chat_photo': False, 'group_chat_created': False, 'supergroup_chat_created': False, 'channel_chat_created': False, 
+'from': {'id': 444610376, 'first_name': 'Jacopo', 'is_bot': False, 'username': 'trelium', 'language_code': 'en'}}} 
+
+print(diction['message']['from']['username'])
+print (u'\U0001f604')
